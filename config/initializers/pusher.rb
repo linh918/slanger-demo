503c081eb33c0efc327a826357ef97570c661d87
Nguyen Dang Duc Linh <linhndd@lapurema.net>
2019-04-16 14:47:06 +0700

    config slanger with rails

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,7 @@
+Pusher.app_id='application_id'
+Pusher.key='application_key'
+Pusher.secret='the_secret_for_the_application'
+Pusher.host='localhost'
+Pusher.port=4567
+Pusher.logger=Rails.logger
+puts "Pusher: ", Pusher.port
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoSlanger::Application.config.secret_key_base = '0b0d862690bca272e30bd53e5f9386756e3d226564f411f9481e932be53dafc4dccf3b4170bcb76a8b04db15c83f99d1fc85d1e0910dda021ec2a47422fbb931'
